c5b894dd33bdcc1ab1fad8b37141b2595eb1ebdd
rasharma-usgbc <rasharma@usgbc.org>
2015-07-23 14:12:34 -0400

    Update django-admin.py

--- eb_django_app/bin/django-admin.py
+++ eb_django_app/bin/django-admin.py
@@ -3,3 +3,6 @@ from django.core import management
 
 if __name__ == "__main__":
     management.execute_from_command_line()
+
+export DJANGO_SETTINGS_MODULE=mysite.settings
+django-admin.py runserver
