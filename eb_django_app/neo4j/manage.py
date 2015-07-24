2ab32848527da2ce51c6728a9335f8062f253046
rasharma-usgbc <rasharma@usgbc.org>
2015-07-24 02:27:40 -0400

    Update manage.py

--- eb_django_app/neo4j/manage.py
+++ eb_django_app/neo4j/manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+from py2neo import neo4j
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neo4j.settings")
