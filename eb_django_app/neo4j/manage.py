8502f0f4bd3191da89d24c0bccd119056262a2eb
rasharma-usgbc <rasharma@usgbc.org>
2015-07-24 02:47:41 -0400

    Update manage.py

--- eb_django_app/neo4j/manage.py
+++ eb_django_app/neo4j/manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-from py2neo import neo4j
+#from py2neo import neo4j
 from neo4jrestclient.client import GraphDatabase
 
-graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+from urlparse import urlparse, urlunparse
+url = urlparse("http://arcdb.sb05.stations.graphenedb.com:24789/db/data/")
+url_without_auth = urlunparse((url.scheme, "{0}:{1}".format(url.hostname, url.port), url.path, None, None, None))
+
+gdb = GraphDatabase(url_without_auth, username = 'arc_db', password = 'EOrf19W9V1DpiNZEwyli')
+
+#graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neo4j.settings")
