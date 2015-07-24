f6632ddf63f60ff0f78f1af6cdf3aa243764262d
rasharma-usgbc <rasharma@usgbc.org>
2015-07-24 02:52:40 -0400

    Update manage.py

--- eb_django_app/neo4j/manage.py
+++ eb_django_app/neo4j/manage.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
+
+from py2neo import ServiceRoot
+graphenedb_url = os.environ.get("http://arcdb.sb05.stations.graphenedb.com:24789/db/data/", "http://localhost:7474/")
+graph = ServiceRoot(graphenedb_url).graph
+
 #from py2neo import neo4j
-from neo4jrestclient.client import GraphDatabase
+#from neo4jrestclient.client import GraphDatabase
 
-from urlparse import urlparse, urlunparse
-url = urlparse("http://arcdb.sb05.stations.graphenedb.com:24789/db/data/")
-url_without_auth = urlunparse((url.scheme, "{0}:{1}".format(url.hostname, url.port), url.path, None, None, None))
+#from urlparse import urlparse, urlunparse
+#url = urlparse("http://arcdb.sb05.stations.graphenedb.com:24789/db/data/")
+#url_without_auth = urlunparse((url.scheme, "{0}:{1}".format(url.hostname, url.port), url.path, None, None, None))
 
-gdb = GraphDatabase(url_without_auth, username = 'arc_db', password = 'EOrf19W9V1DpiNZEwyli')
+#gdb = GraphDatabase(url_without_auth, username = 'arc_db', password = 'EOrf19W9V1DpiNZEwyli')
 
 #graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
